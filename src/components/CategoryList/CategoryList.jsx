3543caf3d9c46174f8313e75775590a7c86d4b16
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-03 16:13:00 +0200

    added categorylist

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+
+class CategoryList extends Component {
+    state = {
+        inputCategory: "",
+    };
+
+    handleInputChange = (e) => {
+        const { value } = e.target;
+        this.setState({ inputCategory: value });
+    };
+
+    handleSubmitNewCategory = (e) => {
+        e.preventDefault();
+        const newCategory = {
+            title: this.state.inputCategory,
+            id: nanoid(),
+        };
+        this.props.addCategory(newCategory);
+        this.reset();
+    };
+    reset = () => {
+        this.setState({ inputCategory: "" });
+    };
+
+    render() {
+        const { categoriesList } = this.props;
+        return (
+            <>
+                <ul>
+                    {categoriesList.map((el) => (
+                        <li key={el.id}>
+                            <p>{el.title}</p>
+                            <button>...</button>
+                            <div>
+                                <button>Delete</button>
+                                <button>Edit</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <form onSubmit={this.handleSubmitNewCategory}>
+                    <input
+                        type="text"
+                        placeholder="new category"
+                        value={this.state.inputCategory}
+                        onChange={this.handleInputChange}
+                    />
+                    <button type="submit">+</button>
+                </form>
+            </>
+        );
+    }
+}
+
+export default CategoryList;
--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,43 +1,110 @@
-const TransactionForm = () => {
-    return (
-        <>
-            <select name="transType" id="">
-                <option value="incomes">Доходы</option>
-                <option value="costs">Расходы</option>
-            </select>
+import { Component } from "react";
+import CategoryList from "../CategoryList/CategoryList";
 
-            <form action="">
-                <label>
-                    День
-                    <input type="date" />
-                </label>
+class TransactionForm extends Component {
+    state = {
+        date: "2022-02-03",
+        time: "",
+        category: "eat",
+        sum: "",
+        currency: "UAH",
+        comment: "",
+        categoriesList: [],
+    };
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    };
+    addCategory = (newCategory) => {
+        this.setState((prevState) => ({
+            categoriesList: [...prevState.categoriesList, newCategory],
+        }));
+    };
 
-                <label>
-                    Время
-                    <input type="time" />
-                </label>
+    handleSubmitTrans = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    };
 
-                <label>
-                    Категория
-                    <input type="button" value="Eat" />
-                </label>
+    render() {
+        const { date, time, category, sum, currency, comment, categoriesList } =
+            this.state;
+        return (
+            <>
+                <select name="transactionType" id="">
+                    <option value="Incomes">Доходы</option>
+                    <option value="Expends">Расходы</option>
+                </select>
+                <form onSubmit={this.handleSubmitTrans}>
+                    <label>
+                        День
+                        <input
+                            type="date"
+                            name="date"
+                            value={date}
+                            onChange={this.handleChange}
+                        />
+                    </label>
 
-                <label>
-                    Сумма
-                    <input placeholder="Введите сумму" type="text" />
-                </label>
+                    <label>
+                        Время
+                        <input
+                            type="time"
+                            name="time"
+                            value={time}
+                            onChange={this.handleChange}
+                        />
+                    </label>
 
-                <label>
-                    Валюта
-                    <input type="button" value="UAH" />
-                </label>
+                    <label>
+                        Категория
+                        <input
+                            type="button"
+                            name="category"
+                            value={category}
+                            onClick={null}
+                        />
+                    </label>
 
-                <label>
-                    <input placeholder="comment" type="comment" />
-                </label>
-            </form>
-        </>
-    );
-};
+                    <label>
+                        Сумма
+                        <input
+                            type="text"
+                            placeholder="Enter sum"
+                            name="sum"
+                            value={sum}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+
+                    <label>
+                        Валюта
+                        <input
+                            type="button"
+                            name="currency"
+                            value={currency}
+                            onClick={null}
+                        />
+                    </label>
+
+                    <label>
+                        <input
+                            type="text"
+                            placeholder="comment"
+                            name="comment"
+                            value={comment}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+                <CategoryList
+                    categoriesList={categoriesList}
+                    addCategory={this.addCategory}
+                />
+            </>
+        );
+    }
+}
 
 export default TransactionForm;
