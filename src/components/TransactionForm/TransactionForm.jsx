052c4d685f71d2187ec6e4003ed7ad6ab9c00dc8
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-10 16:26:17 +0200

    refactored on hooks TransactionForm-component

--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,151 +1,139 @@
-import { nanoid } from "nanoid";
-import { Component } from "react";
-import {postTransaction} from "../../api";
+import { useState } from "react";
+import { postTransaction } from "../../api";
 import CategoryList from "../CategoryList/CategoryList";
 
-class TransactionForm extends Component {
-  state = {
-    date: "2022-02-22",
-    time: "",
-    category: "eat",
-    currency: "UAH",
-    comment: "",
-    total: "",
-    categoriesList: [
-      { id: 1, title: "Eat" },
-      { id: 2, title: "Drink" },
-    ],
-    transType: "costs",
-  };
+const initialForm = {
+  date: "2022-02-22",
+  time: "",
+  category: "eat",
+  currency: "UAH",
+  comment: "",
+  total: ""
+};
 
-  handleChange = (e) => {
+const initialCategoriesList = [
+  { id: 1, title: "Eat" },
+  { id: 2, title: "Drink" },
+];
+
+const TransactionForm = ({ isOpenCategories, togleCategoryList, addTransaction }) => {
+
+  const [form, setForm] = useState(initialForm);
+  const [categoriesList, setCategoriesList] = useState(initialCategoriesList);
+  const [transType, setTransType] = useState("costs");
+
+  const handleChangeForm = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    setForm(prev => ({ ...prev, [name]: value }));
   };
 
-  addCategory = (newCategory) => {
-    this.setState((prevState) => ({
-      categoriesList: [...prevState.categoriesList, newCategory],
-    }));
-  };
-  handleSubmitTrans = (e) => {
-    e.preventDefault();
-    const { categoriesList, ...transaction } = this.state;
-    transaction.id = nanoid();
-    postTransaction({ transType:transaction.transType, transaction }).then((data) =>
-      this.props.addTransaction(data)
-    );
-    this.reset();
-  };
-
-  reset = () => {
-    const resetedState = Object.keys(this.state).reduce((acc, el) => {
-      if (el === "categoriesList") return acc;
-      if (el === "category") {
-        acc[el] = "Eat";
-        return acc;
-      }
-      if (el === "date") {
-        acc[el] = "2022-02-22";
-        return acc;
-      }
-      acc[el] = "";
-      return acc;
-    }, {});
-    this.setState(resetedState);
-  };
-
-  setCategory = (newCategory) => {
-    this.setState({ category: newCategory });
-    this.props.togleCategoryList();
-  };
-
-  render() {
-    const { data, time, category, total, currency, comment, categoriesList } =
-      this.state;
-    const { isOpenCategories, togleCategoryList } = this.props;
-    return (
-      <>
-        {!isOpenCategories ? (
-          <>
-            <select name="transType" onChange={this.handleChange} value={this.state.transType}>
-              <option value="incomes">Incomes</option>
-              <option value="costs">Costs</option>
-            </select>
-            <form onSubmit={this.handleSubmitTrans}>
-              <label>
-                Day
-                <input
-                  name="date"
-                  type="date"
-                  value={data}
-                  onChange={this.handleChange}
-                />
-              </label>
-
-              <label>
-                Time
-                <input
-                  name="time"
-                  type="time"
-                  value={time}
-                  onChange={this.handleChange}
-                />
-              </label>
-
-              <label>
-                Category
-                <input
-                  name="category"
-                  type="button"
-                  value={category}
-                  onClick={togleCategoryList}
-                />
-              </label>
-
-              <label>
-                Total
-                <input
-                  name="total"
-                  type="text"
-                  placeholder="Enter sum"
-                  value={total}
-                  onChange={this.handleChange}
-                />
-              </label>
-
-              <label>
-                Currency
-                <input
-                  name="currency"
-                  type="button"
-                  value={currency}
-                  onClick={null}
-                />
-              </label>
-
-              <label>
-                <input
-                  name="comment"
-                  type="text"
-                  placeholder="Comment"
-                  value={comment}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <button type="submit">Submit</button>
-            </form>
-          </>
-        ) : (
-          <CategoryList
-            categoriesList={categoriesList}
-            addCategory={this.addCategory}
-            togleCategoryList={togleCategoryList}
-            setCategory={this.setCategory}
-          />
-        )}
-      </>
-    );
+  const handleChangeTransType = (e) => {
+    const { value } = e.target;
+    setTransType(value)
   }
+
+  const addCategory = (newCategory) => {
+    setCategoriesList(prevCategoryList => [...prevCategoryList, newCategory])
+  };
+
+  const handleSubmitTrans = (e) => {
+    e.preventDefault();
+    postTransaction({ transType, transaction: { ...form, transType } }).then((data) =>
+      addTransaction(data)
+    );
+    setForm(initialForm);
+  };
+
+  const setCategory = (newCategory) => {
+    setForm(prevForm => ({ ...prevForm, category: newCategory }));
+    togleCategoryList();
+  };
+
+
+  const { data, time, category, total, currency, comment } =
+    form;
+
+  return (
+    <>
+      {!isOpenCategories ? (
+        <>
+          <select name="transType" onChange={handleChangeTransType} value={transType}>
+            <option value="incomes">Incomes</option>
+            <option value="costs">Costs</option>
+          </select>
+          <form onSubmit={handleSubmitTrans}>
+            <label>
+              Day
+              <input
+                name="date"
+                type="date"
+                value={data}
+                onChange={handleChangeForm}
+              />
+            </label>
+
+            <label>
+              Time
+              <input
+                name="time"
+                type="time"
+                value={time}
+                onChange={handleChangeForm}
+              />
+            </label>
+
+            <label>
+              Category
+              <input
+                name="category"
+                type="button"
+                value={category}
+                onClick={togleCategoryList}
+              />
+            </label>
+
+            <label>
+              Total
+              <input
+                name="total"
+                type="text"
+                placeholder="Enter sum"
+                value={total}
+                onChange={handleChangeForm}
+              />
+            </label>
+
+            <label>
+              Currency
+              <input
+                name="currency"
+                type="button"
+                value={currency}
+                onClick={null}
+              />
+            </label>
+
+            <label>
+              <input
+                name="comment"
+                type="text"
+                placeholder="Comment"
+                value={comment}
+                onChange={handleChangeForm}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      ) : (
+        <CategoryList
+          categoriesList={categoriesList}
+          addCategory={addCategory}
+          togleCategoryList={togleCategoryList}
+          setCategory={setCategory}
+        />
+      )}
+    </>
+  );
 }
 export default TransactionForm;
